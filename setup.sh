060d2f872edad444bc93b018cdc3ef4b74d6a652
PilliSiddharth <39644724+PilliSiddharth@users.noreply.github.com>
2020-07-29 14:27:54 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p .streamlit/
-echo "[general]" > .streamlit/credentials.toml
-echo "email="\" >> .streamlit/credentials.toml
-echo "[server]" > .streamlit/config.toml
-echo "headless=true" >> .streamlit/config.toml
-echo "enableCORS=false" >> .streamlit/config.toml
-echo "port=$PORT" >> .streamlit/config.toml
+echo "[general]" > ~/.streamlit/credentials.toml
+echo "email="\" >> ~/.streamlit/credentials.toml
+echo "[server]" > ~/.streamlit/config.toml
+echo "headless=true" >> ~/.streamlit/config.toml
+echo "enableCORS=false" >> ~/.streamlit/config.toml
+echo "port=$PORT" >> ~/.streamlit/config.toml
