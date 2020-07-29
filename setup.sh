a48cf50e72b01ab3d6f063473776eeb4e3ab3367
developer <developer@montaigne.co>
2020-07-29 16:12:48 +0530

    Update dependecies
    Update Procfile
    
    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
-mkdir -p .streamlit/
+mkdir -p ~/.streamlit/
+
 echo "[general]" > ~/.streamlit/credentials.toml
-echo "email="\" >> ~/.streamlit/credentials.toml
+echo "email=developer@montaigne.co" >> ~/.streamlit/credentials.toml
+
 echo "[server]" > ~/.streamlit/config.toml
 echo "headless=true" >> ~/.streamlit/config.toml
 echo "enableCORS=false" >> ~/.streamlit/config.toml
